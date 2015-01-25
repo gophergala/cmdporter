62d0ed34ea750486f6a4d801919e800a8560b00c
Zevran <info@zevran.net>
2015-01-25 18:12:55 +0100

    Implement cmd route on post request

--- cmdporter.go
+++ cmdporter.go
@@ -18,14 +18,30 @@ x load commands params from file
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"path"
 	"text/template"
+
+	"github.com/gophergala/cmdporter/vp/nec"
 )
 
-var SerialPortStatus bool = false
+var (
+	SerialPortStatus bool = false
+	g_Device         Device
+)
+
+type CmdRequest struct {
+	Command string `json:"command"`
+}
+
+type CmdResponse struct {
+	Data  interface{} `json:"data"`
+	Error interface{} `json:"error"`
+}
 
 func Render(w http.ResponseWriter, view string, content interface{}) {
 	layout, err := ioutil.ReadFile(path.Join("views", "layout.html"))
@@ -51,43 +67,56 @@ func Render(w http.ResponseWriter, view string, content interface{}) {
 
 }
 
-func main() {
+func ParseBody(r *http.Request) []byte {
+	body, err := ioutil.ReadAll(r.Body)
 
-	devices := []string{
-		"Nec mg271wg",
-		"Arduino One",
+	if err != nil {
+		fmt.Println(err)
 	}
+
+	return body
+}
+
+func main() {
+	g_Device = nec.Nec_m271_m311
+
+	nec.Nec_m271_m311.Load()
 
 	// Start Http Server
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		content := map[string]interface{}{
 			"SerialPortStatus": SerialPortStatus,
-			"Devices":          devices,
+			"Device":           g_Device.GetName(),
 		}
 
 		Render(w, "index.html", content)
 	})
 
-	http.HandleFunc("/devices", func(w http.ResponseWriter, r *http.Request) {
-		content := map[string]interface{}{
-			"Devices": devices,
-		}
-
-		Render(w, "devices.html", content)
-	})
-
-	http.HandleFunc("/device/", func(w http.ResponseWriter, r *http.Request) {
-		content := map[string]interface{}{
-			"Name": r.URL.Path[8:],
-		}
-
-		Render(w, "device.html", content)
-	})
-
 	http.HandleFunc("/cmd", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
+			req := CmdRequest{}
+			res := CmdResponse{nil, nil}
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
+			if err := json.Unmarshal(ParseBody(r), &req); err != nil {
+				fmt.Println(err)
+			}
+
+			// Search for submited command on device
+			if ok := g_Device.Commands[value.Name]; ok != nil {
+				fmt.Printf("Found command : %s => %v\n", value.Name, value.Bytes)
+				res.Data = "Success"
+				jsonRes, _ := json.Marshal(res)
+				fmt.Fprintf(w, "%s", string(jsonRes))
+				return
+			}
+
+			// Command not found in device commands list
+			res.Error = "CommandNotFound"
+			jsonRes, _ := json.Marshal(res)
+			fmt.Fprintf(w, "%s", string(jsonRes))
+			return
 		}
 		w.WriteHeader(http.StatusNotFound)
 	})
